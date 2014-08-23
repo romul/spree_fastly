6afa1ac70593446e6a40526f8555b9776de41b24
Denis Ivanov <visible@jumph4x.net>
2014-08-22 19:52:32 -0700

    Naive HomeController header spec

--- spec/controllers/spree/home_controller_spec.rb
+++ spec/controllers/spree/home_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe Spree::HomeController do
   it "responds with success" do
     spree_get :index
+
     response.should be_cacheable
+    response.should have_surrogate_keys('products')
+    response.should have_surrogate_keys('taxonomies')
+    response.should have_surrogate_keys('taxons')
   end
 end
