b5bc33226263e8130f50c1d5d069ca7b44e82a7b
Denis Ivanov <visible@jumph4x.net>
2014-08-27 21:25:39 -0700

    Context blocks for HomeController spec

--- spec/controllers/spree/home_controller_spec.rb
+++ spec/controllers/spree/home_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe Spree::HomeController do
-  it "responds with success" do
-    spree_get :index
+  describe 'on #index' do
+    it "sets correct headers" do
+      spree_get :index
 
-    expect(response).to be_cacheable
-    expect(response).to have_surrogate_keys('products')
-    expect(response).to have_surrogate_keys('taxonomies')
-    expect(response).to have_surrogate_keys('taxons')
+      expect(response).to be_cacheable
+      expect(response).to have_surrogate_keys('products')
+      expect(response).to have_surrogate_keys('taxonomies')
+      expect(response).to have_surrogate_keys('taxons')
+    end
   end
 end
