0b96bb977e249ed7a3e791de707044315ab99f5b
Roman Smirnov <POMAHC@gmail.com>
2014-10-10 18:34:15 +0400

    Don't use get params as surrogate keys

--- app/controllers/spree/api/products_controller_decorator.rb
+++ app/controllers/spree/api/products_controller_decorator.rb
@@ -18,7 +18,6 @@ module Spree
           surrogate_keys = keys_for_collections(:products)
           surrogate_keys += @products.map(&:record_key) if @products
           surrogate_keys << @product.record_key if @product
-          surrogate_keys << request.query_string if request.query_string.present?
           set_surrogate_key_header surrogate_keys
         end
         super
