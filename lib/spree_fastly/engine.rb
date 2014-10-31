cbbe450906e272e609a25230edc2b7457a8c86d5
Denis I <visible@jumph4x.net>
2014-10-31 16:01:56 -0700

    Enable static file downloads

--- lib/spree_fastly/engine.rb
+++ lib/spree_fastly/engine.rb
@@ -9,6 +9,11 @@ module SpreeFastly
       g.test_framework :rspec
     end
 
+    # Initializer to combine this engines static assets with the static assets of the hosting site.
+    initializer "static assets" do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+    end
+
     def self.activate
       Spree::StoreController.send :include, SpreeFastly::ControllerHelpers
       Spree::Api::BaseController.send :include, SpreeFastly::ControllerHelpers
